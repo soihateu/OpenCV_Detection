8e6114427edb9a674e045de1acf89898b364c47b
soihateu <ericsgjames@gmail.com>
2020-11-11 22:52:02 -0500

    changes

--- opencv_detection.py
+++ opencv_detection.py
@@ -1,46 +1,12 @@
 import cv2
-import numpy as np
-from datetime import datetime
 
-cap = cv2.VideoCapture('Traffic.avi')
-frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+c = cv2.VideoCapture('Traffic.mp4')
+cc = cv2.CascadeClassifier('cars.xml')
 
-frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
-
-fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
-
-ret, frame1 = cap.read()
-ret, frame2 = cap.read()
-
-dateTimeObj = datetime.now()
-print(frame1.shape + dateTimeObj)
-while cap.isOpened():
-    diff = cv2.absdiff(frame1, frame2)
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-    dilated = cv2.dilate(thresh, None, iterations=3)
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    for contour in contours:
-        (x, y, w, h) = cv2.boundingRect(contour)
-
-        if cv2.contourArea(contour) < 900:
-            continue
-        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (0, 0, 255), 3)
-    #cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
-
-    image = cv2.resize(frame1, (1280,720))
-    out.write(image)
-    cv2.imshow("feed", frame1)
-    frame1 = frame2
-    ret, frame2 = cap.read()
-
-    if cv2.waitKey(40) == 27:
+while True:
+    ret, f = c.read()
+    if not ret:
         break
 
-cv2.destroyAllWindows()
-cap.release()
-out.release()
+    if len(cc.detectMultiScale(cv2.cvtColor(f, cv2.COLOR_BGR2GRAY), 1.1, 9)) > 0:
+        print("At this time of the video: " + str(c.get(cv2.CAP_PROP_POS_MSEC)))
