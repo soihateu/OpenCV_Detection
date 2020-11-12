da07a77640bda94fc9ac247cac595e3a1da3d95a
soihateu <ericsgjames@gmail.com>
2020-11-11 23:29:39 -0500

    first commit

--- opencv_detection.py
+++ opencv_detection.py
@@ -1,22 +1,12 @@
 import cv2
 
-my_cascade = cv2.CascadeClassifier('toy.xml')
+c = cv2.VideoCapture('Traffic.mp4')
+cc = cv2.CascadeClassifier('cars.xml')
 
-def detect(gray, frame):
-    toys= my_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in toys:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        print("Toy Detected at: "+str(video_capture.get(cv2.CAP_PROP_POS_MSEC)))
-    return frame 
-
-video_capture = cv2.VideoCapture('home.mp4')
-cv2.startWindowThread()
-while True: 
-    _, frame = video_capture.read() 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-    canvas = detect(gray, frame) 
-    cv2.imshow('Video', canvas) 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+while True:
+    ret, f = c.read()
+    if not ret:
         break
-video_capture.release()
-cv2.destroyAllWindows()
+
+    if len(cc.detectMultiScale(cv2.cvtColor(f, cv2.COLOR_BGR2GRAY), 1.1, 9)) > 0:
+        print("At this time of the video: " + str(c.get(cv2.CAP_PROP_POS_MSEC)))
