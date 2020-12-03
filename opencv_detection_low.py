4486089a15771ebf4d1a83e491301868c6e0e798
Eric Tsim <32168645+soihateu@users.noreply.github.com>
2020-12-03 14:20:43 -0500

    Update opencv_detection_low.py

--- opencv_detection_low.py
+++ opencv_detection_low.py
@@ -3,10 +3,10 @@ import cv2
 c = cv2.VideoCapture('Traffic_low.mp4')
 cc = cv2.CascadeClassifier('cars.xml')
 
-while True
+while True:
     ret, f = c.read()
-    if not ret
+    if not ret:
         break
 
-    if len(cc.detectMultiScale(cv2.cvtColor(f, cv2.COLOR_BGR2GRAY), 1.1, 9))  0
+    if len(cc.detectMultiScale(cv2.cvtColor(f, cv2.COLOR_BGR2GRAY), 1.1, 9)) > 0:
         print(At this time of the video  + str(c.get(cv2.CAP_PROP_POS_MSEC)))
